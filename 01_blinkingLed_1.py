19d3257b0c54432b5753b1b602db7464b1298de7
adeept@163.com <adeept@163.com>
2015-06-12 11:24:32 +0000

    add comment

--- 01_blinkingLed_1.py
+++ 01_blinkingLed_1.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+
+#-----------------------------------------------------------
+# File name   : blinkingLed.py
+# Description : make an led blinking.
+# Author      : Jason
+# E-mail      : jason@adeept.com
+# Website     : www.adeept.com
+# Date        : 2015/06/12
+#-----------------------------------------------------------
+
 import RPi.GPIO as GPIO
 import time
 
